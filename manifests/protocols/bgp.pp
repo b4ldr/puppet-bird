c94132b9028c66e1ba6449a4860c9169403d6647
b4ldr <github@johnbond.org>
2015-12-11 18:04:03 +0000

    add ipv4/ipv6 options

--- manifests/protocols/bgp.pp
+++ manifests/protocols/bgp.pp
@@ -34,11 +34,20 @@ define bird::protocols::bgp (
   validate_bool($passive)
   validate_bool($rs_client)
   validate_hash($neighbours)
+  if $bird::ipv4_enable and $bird::ipv6_enable {
+    $notify = Service[$bird::v4_service, $bird::v6_service]
+  } elsif $bird::ipv4_enable {
+    $notify = Service[$bird::v4_service]
+  } elsif $bird::ipv6_enable {
+    $notify = Service[$bird::v6_service]
+  } else {
+    fail('ipv4 or ipv6 must be enabled')
+  }
 
   file { "${bird::config_dir}/templates/bgp-${name}.conf":
     ensure  => present,
     content => template('bird/etc/bird/templates/bgp.conf.erb'),
-    notify  => Service[$bird::v4_service, $bird::v6_service],
+    notify  => $notify,
   }
   create_resources('bird::protocols::bgp::neighbour', $neighbours)
   Bird::Protocols::Bgp::Neighbour[keys($neighbours)] { template => $name }
