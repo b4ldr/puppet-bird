cdc08aa41f46dd74116732eb6b739dafe84a07a5
b4ldr <github@johnbond.org>
2015-12-16 19:39:37 +0000

    change export filter of direct

--- manifests/protocols/direct.pp
+++ manifests/protocols/direct.pp
@@ -4,7 +4,7 @@ define bird::protocols::direct (
   $table         = undef,
   $debug         = 'off',
   $import_filter = 'all',
-  $export_filter = 'none',
+  $export_filter = 'all',
   $interfaces    = [],
 ) {
   if $table {
@@ -13,11 +13,11 @@ define bird::protocols::direct (
   validate_re($debug,'^(all|off|states|routes|filters|events|packets)$')
   validate_string($import_filter)
   validate_string($export_filter)
-  if ! ($import_filter in ['all', 'none']) 
+  if ! ($import_filter in ['all', 'none'])
       and ! defined(Bird::Filter[$import_filter]) {
     fail("you must define bird::filter['${import_filter}']")
   }
-  if ! ( $export_filter in ['all', 'none']) 
+  if ! ( $export_filter in ['all', 'none'])
       and ! defined(Bird::Filter[$import_filter]) {
     fail("you must define bird::filter['${export_filter}']")
   }
